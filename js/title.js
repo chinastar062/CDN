6cf0a18eff6e00c32db80f774a9a6c6fd2882005
若葉·凌柚 <52341338+RomEYer-AwELF@users.noreply.github.com>
2020-04-28 16:42:21 +0800

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,14 @@
+    var title = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function() {
+        if (document.hidden) {
+            document.title = '(つェ⊂)我藏好了哦~ ';
+            clearTimeout(titleTime);
+        }
+        else {
+            document.title = '(*´∇｀*) 被你发现啦~ ';
+            titleTime = setTimeout(function() {
+                document.title = title;
+            }, 2000);
+        }
+    });
