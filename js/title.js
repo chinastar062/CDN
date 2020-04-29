e473f3694f3e2fafd43c71aa3cda87157d367cac
若葉·凌柚 <52341338+RomEYer-AwELF@users.noreply.github.com>
2020-04-29 13:52:34 +0800

    Update title.js

--- js/title.js
+++ js/title.js
@@ -12,3 +12,12 @@
             }, 2000);
         }
     });
+   router.beforeEach((to, from, next) => {
+       /* 路由发生变化修改页面title */
+       if (to.meta.title) {
+           document.title = to.meta.title;
+       }else{
+           document.title = '默认title';
+       }
+       next();
+   })
