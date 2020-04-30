cae0ab3be563ba797574f091f8490a8c97e2c7e9
若葉·凌柚 <52341338+RomEYer-AwELF@users.noreply.github.com>
2020-04-30 14:31:27 +0800

    Update console.js

--- js/console.js
+++ js/console.js
@@ -2,6 +2,6 @@ if (window.console) {
     var cons = console;
     if (cons) {
         console.log("%c\n     ","font-size:200px;background:url('https://cdn.jsdelivr.net/gh/RomEYer-AwELF/CDN/img/D.png') no-repeat 0 0");
-        console.log("%c\n如果你看到了这个，你大概就可以知道了,这个愚蠢而且懒惰的博主仍然在划水。\n\n而且代码依然乱七八糟！！\n---By RomEYer"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:15px")
+        console.log("%c\唔.....果然这里会一直报错呢，为什么呢？"," text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #FE9600,0 4px 0 #b9b9b9,0 5px 0 #FE9600,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:15px;font-family:loli")
     }
 }
